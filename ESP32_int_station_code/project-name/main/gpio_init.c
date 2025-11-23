caace992a63139f211c13ac124d14c29db742036
SzymonDobrowolski <szymon.dobrowolski@student.put.poznan.pl>
2025-11-23 14:22:15 +0100

    kod do esp

---
+++ ESP32_int_station_code/project-name/main/gpio_init.c
@@ -0,0 +1,6 @@
+#include "driver/gpio.h"
+#include "gpio_init.h"
+void gpio_init(void)
+{
+    // GPIO initialization code here
+}
---
+++ ESP32_int_station_code/project-name/main/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "gpio_init.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+void app_main(void)
+{
+    gpio_init();
+}
