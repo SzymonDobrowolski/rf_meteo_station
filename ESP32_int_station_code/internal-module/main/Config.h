bc5d241c4b378915c66e2ff93005c4482ee34817
Rafal Ryske <r.ryske2001@gmail.com>
2026-01-29 21:19:42 +0100

    Dodano wykres, zmieniono napisy

---
+++ ESP32_int_station_code/internal-module/main/Config.h
@@ -0,0 +1,43 @@
+#pragma once
+#define BUTTON_1 GPIO_NUM_36
+#define BUTTON_2 GPIO_NUM_39
+
+#define LED1 GPIO_NUM_13
+#define LED2 GPIO_NUM_21
+
+// SPI Pins
+#define SPI_MISO GPIO_NUM_19
+#define SPI_MOSI GPIO_NUM_23
+#define SPI_SCLK GPIO_NUM_18
+
+// NRF Pins
+#define NRF_CE_PIN GPIO_NUM_22
+#define NRF_CSN_PIN GPIO_NUM_16
+//NRF Settings
+#define RX_ADDRESS_VAL {'A', 'T', '4', '1', '4'}
+#define Channel 76
+#define NRF_SPEED 0x07 //2Mbps, 0dBm
+
+// --- STRUKTURA DANYCH (12 bajtów) ---
+typedef struct __attribute__((packed)) {
+    int32_t temp_hundredths;
+    uint32_t pressure_pa;
+    uint32_t hum_x1024;
+} SensorData;
+
+// LCD Pins
+#define CS_PIN GPIO_NUM_4
+#define DC_PIN GPIO_NUM_26
+#define RESET_PIN GPIO_NUM_25
+#define BL_PIN GPIO_NUM_27
+//Optional LCD
+#define T_CS GPIO_NUM_14
+#define SD_CS GPIO_NUM_17
+
+#define NRF_IRQ GPIO_NUM_34
+#define T_IRQ GPIO_NUM_35
+
+// NTC
+#define SSID "POCO X3 NFC"
+#define PASSWORD "Rafik123"
+#define SNTP_SERVER "pool.ntp.org"
